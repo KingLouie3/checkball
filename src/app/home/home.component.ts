7feb2622d0814e539e3a67dd518af9a55fd71c90
Louis Sutton <louis.sutton.iii@gmail.com>
2020-02-07 20:12:57 -0800

    Added stat variables and fixed chart

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { BasketballApiService } from "./../basketball-api.service";
 import { FormsModule } from "@angular/forms";
-import {Datum}  from  'src/stats';
+import { Datum } from "src/stats";
 
 import {
   Component,
@@ -17,7 +17,8 @@ import { HttpClientModule, HttpClient } from "@angular/common/http";
   styleUrls: ["./home.component.scss"]
 })
 export class HomeComponent {
-  canvas:  any;
+  show: boolean = true;
+  canvas: any;
   ctx: any;
   players = [
     { name: "Lebron" },
@@ -32,6 +33,25 @@ export class HomeComponent {
   ppg;
   reb;
   ast;
+  games_played;
+  min;
+  fgm;
+  fga;
+  fg3m;
+  fg3a;
+  ftm;
+  fta;
+  oreb;
+  dreb;
+  stl;
+  blk;
+  pf;
+  fg_pct;
+  fg3_pct;
+  ft_pct;
+  turnover;
+  first_name;
+  last_name;
 
   constructor(public _api: BasketballApiService, private _http: HttpClient) {}
 
@@ -42,10 +62,12 @@ export class HomeComponent {
           `https://www.balldontlie.io/api/v1/players?season[]=2019&search=${player}`
         )
         .subscribe(response => {
+          this.first_name =  response.data.first_name;
           this.suggestions = response.data.filter(player => {
+            
             return player.id <= 400;
           });
-          console.log(this.suggestions);
+          console.log('lets see', response);
         });
 
       console.log(player);
@@ -63,40 +85,56 @@ export class HomeComponent {
         this.ppg = response.data[0].pts;
         this.reb = response.data[0].reb;
         this.ast = response.data[0].ast;
+        this.games_played = response.data[0].games_played;
+        this.oreb = response.data[0].oreb;
+        this.reb = response.data[0].dreb;
+        this.fgm = response.data[0].fgm;
+        this.fga = response.data[0].fga;
+        this.fg_pct = response.data[0].fg_pct;
+        this.fg3m = response.data[0].fg3m;
+        this.fg3a = response.data[0].fg3a;
+        this.fg3_pct = response.data[0].fg3_pct;
+        this.ftm = response.data[0].ftm;
+        this.fta = response.data[0].fta;
+        this.ft_pct = response.data[0].ft_pct;
+        this.stl = response.data[0].stl;
+        this.pf = response.data[0].pf;
+        this.min = response.data[0].min;
 
-        console.log("points",this.ppg)
-        
-          this.canvas = document.getElementById('myChart');
-          this.ctx = this.canvas.getContext('2d');
-          let myChart = new Chart(this.ctx, {
-            type: 'line',
-            data: {
-                labels: ["Points", "Rebounds", "Assists"],
-                datasets: [{
-                    label: '# of Votes',
-                    data: [this.ppg,this.reb,this.ast],
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)'
-                    ],
-                    borderWidth: 1
-                }]
-            },
-            options: {
-              responsive: false,
-              display:true
-            }
-          });
+        this.turnover = response.data[0].turnover;
+        console.log("points", this.ppg);
+        console.log("games", this.games_played);
 
-      })
-      
+        this.canvas = document.getElementById("myChart");
+        this.ctx = this.canvas.getContext("2d");
+        let myChart = new Chart(this.ctx, {
+          type: "line",
+          data: {
+            labels: ["Points", "Rebounds", "Assists"],
+            datasets: [
+              {
+                label: "# of Votes",
+                data: [this.ppg, this.reb, this.ast],
+                backgroundColor: [
+                  "rgba(255, 99, 132, 1)",
+                  "rgba(54, 162, 235, 1)",
+                  "rgba(255, 206, 86, 1)"
+                ],
+                borderWidth: 1
+              }
+            ]
+          },
+          options: {
+            responsive: false,
+            display: true
+          }
+        });
+      });
   }
 
-  
-
-
-
-
-
+  clearSearch(input) {
+    if (input == "") {
+      return (this.show = false);
+    }
+  }
 }
